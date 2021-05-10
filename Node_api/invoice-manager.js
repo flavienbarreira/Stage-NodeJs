0e907c8a09b9d7ffbfc2d12ebc22394864b07325
flavienbarreira <45532042+flavienbarreira@users.noreply.github.com>
2021-05-10 16:25:53 +0200

    Génération + affichage du PDF

--- Node_api/invoice-manager.js
+++ Node_api/invoice-manager.js
@@ -1,69 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+var express = require('express'),
+        app = express(),
+        pdf = require('express-pdf');
+        path = require('path');
+ 
+//previously app.use(pdf())
+app.use(pdf); // or you can app.use(require('express-pdf'));
 
 app.get('/invoice/:firstname/:lastname', (req, res) => {
-  res.statusCode = 200
-  var firstname = req.params.firstname;
-  var lastname = req.params.lastname;
-  
-  
-  var fonts = {
-    Courier: {
-      normal: 'Courier',
-      bold: 'Courier-Bold',
-      italics: 'Courier-Oblique',
-      bolditalics: 'Courier-BoldOblique'
-    },
-    Helvetica: {
-      normal: 'Helvetica',
-      bold: 'Helvetica-Bold',
-      italics: 'Helvetica-Oblique',
-      bolditalics: 'Helvetica-BoldOblique'
-    },
-    Times: {
-      normal: 'Times-Roman',
-      bold: 'Times-Bold',
-      italics: 'Times-Italic',
-      bolditalics: 'Times-BoldItalic'
-    },
-    Symbol: {
-      normal: 'Symbol'
-    },
-    ZapfDingbats: {
-      normal: 'ZapfDingbats'
-    }
-  };
-  
-  var PdfPrinter = require('pdfmake');
-  var printer = new PdfPrinter(fonts);
-  var fs = require('fs');
-  
-  var docDefinition = {
-    content: [
-      "Hello ",
-      firstname,
-      " ",
-      lastname,
-    ],
-    defaultStyle: {
-      font: 'Helvetica'
-    }
-  };
-  
-  var pdfDoc = printer.createPdfKitDocument(docDefinition);
-  pdfDoc.pipe(fs.createWriteStream('document.pdf'));
-  /*var file = fs.createReadStream('document.pdf');
-  var stat = fs.statSync('document.pdf');
-  res.setHeader('Content-Length', stat.size);
-  res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
-  //file.pipe(res);*/
-  pdfDoc.end();
-  //res()
-  console.log(`cc`);
-});
+    res.statusCode = 200
+    var firstname = req.params.firstname;
+    var lastname = req.params.lastname;
+    
+    var fonts = {
+      Times: {
+        normal: 'Times-Roman',
+        bold: 'Times-Bold',
+        italics: 'Times-Italic',
+        bolditalics: 'Times-BoldItalic'
+      },
+    };
+    
+    var PdfPrinter = require('pdfmake');
+    var printer = new PdfPrinter(fonts);
+    var fs = require('fs');
+    
+    var docDefinition = {
+      content: [
+        "Hello " + firstname + " " + lastname,
+      ],
+      defaultStyle: {
+        font: 'Times'
+      }
+    };
+    
+    var pdfDoc = printer.createPdfKitDocument(docDefinition);
+    pdfDoc.pipe(fs.createWriteStream('document.pdf'));
+    pdfDoc.end();
+
+    res.pdf(path.resolve(__dirname, './document.pdf'));
+  });
+
+app.listen(3000);
